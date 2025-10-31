222a9ca73d6bbd4360cdb80e11422060ecee0b03
Joseph Janssen <48490003+joej1997@users.noreply.github.com>
2025-10-31 13:13:28 -0700

    Add files via upload

--- process_lvl3_data.R
+++ process_lvl3_data.R
@@ -81,3 +81,162 @@ write.csv(GRACE_ALL,paste0("C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSC
 
 
 
+
+
+
+
+# process CSR GRACE data. downloaded from https://search.earthdata.nasa.gov/
+
+rm(list = ls())
+library(ncdf4)
+library(ncdf4.helpers)
+library(lubridate)
+library(infotheo)
+library(sf)
+library(sp)
+library(jtools)
+library(lubridate)
+
+
+filez1<-list.files(path="C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSCALE/DATA/TELLUS_GRAC_L3_CSR_RL06_LND_v04_RL06v04-20251031_180235",pattern = "*.nc",full.names = T)
+filez2<-list.files(path="C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSCALE/DATA/TELLUS_GRFO_L3_CSR_RL06.3_LND_v04_RL06.3v04-20251031_194603",pattern = "*.nc",full.names = T)
+filez<-c(filez1,filez2)
+
+
+GRACE_ALL<-NULL
+for(i in 1:length(filez)){
+  currentWeatherdata<-nc_open(filez[i])
+  
+  londata<-ncvar_get(currentWeatherdata,varid = "lon_bounds")
+  latdata<-ncvar_get(currentWeatherdata,varid = "lat_bounds")
+  timedata<-ncvar_get(currentWeatherdata,varid = "time_bounds")
+  lwe<-ncvar_get(currentWeatherdata,varid = "lwe_thickness")
+  uncertainty<-ncvar_get(currentWeatherdata,varid = "uncertainty")
+  nc_close(currentWeatherdata)
+  
+  numdays<-timedata[2]-timedata[1]
+  cur_seq<-seq(from=as.Date("2002-01-01"),by="day",length.out=mean(timedata))
+  monthvec<-month(cur_seq[length(cur_seq)])
+  yearvec<-year(cur_seq[length(cur_seq)])
+  
+  londata[londata>180]<- londata[londata>180] - 360
+  
+  
+  whichlon<-which((londata[2,]>= -130 & londata[2,]<= -105) | (londata[1,]>= -130 & londata[1,]<= -105))
+  whichlat<-which((latdata[2,]>= 25 & latdata[2,]<= 50) | (latdata[1,]>= 25 & latdata[1,]<= 50))
+  lonvec<-londata[1,whichlon]
+  latvec<-latdata[1,whichlat]
+  posinfo<-expand.grid(latvec,lonvec)
+  
+  colnames(posinfo)<-c("min_lat","min_lon")
+  posinfo$max_lat<-posinfo$min_lat+1
+  posinfo$max_lon<-posinfo$min_lon+1
+  posinfo$lwe<-NA
+  posinfo$uncertainty<-NA
+  posinfo$scale_fac<-NA
+  posinfo$month<-NA
+  posinfo$year<-NA
+  posinfo$numdays<-NA
+  
+  for(j in 1:nrow(posinfo)){
+    curlonpos<-which(londata[1,]==posinfo$min_lon[j])
+    curlatpos<-which(latdata[1,]==posinfo$min_lat[j])
+    
+    posinfo$lwe[j]<-lwe[curlonpos,curlatpos]
+    posinfo$uncertainty[j]<-uncertainty[curlonpos,curlatpos]
+    posinfo$month[j]<-monthvec
+    posinfo$year[j]<-yearvec
+    posinfo$numdays[j]<-numdays
+  }
+    
+    
+  GRACE_ALL<-rbind(GRACE_ALL,posinfo)
+  write.csv(posinfo,paste0("C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSCALE/DATA/CSR_MONTHLY/GRACE_CSR_month_",i,".csv"),row.names = F)
+  print(i)
+
+}
+
+write.csv(GRACE_ALL,paste0("C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSCALE/DATA/GRACE_CSR_all.csv"),row.names = F)
+
+
+
+
+
+# process GFZ GRACE data. downloaded from https://search.earthdata.nasa.gov/
+
+rm(list = ls())
+library(ncdf4)
+library(ncdf4.helpers)
+library(lubridate)
+library(infotheo)
+library(sf)
+library(sp)
+library(jtools)
+library(lubridate)
+
+
+filez1<-list.files(path="C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSCALE/DATA/TELLUS_GRAC_L3_GFZ_RL06_LND_v04_RL06v04-20251031_165814",pattern = "*.nc",full.names = T)
+filez2<-list.files(path="C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSCALE/DATA/TELLUS_GRFO_L3_GFZ_RL06.3_LND_v04_RL06.3v04-20251031_173627",pattern = "*.nc",full.names = T)
+filez<-c(filez1,filez2)
+
+
+GRACE_ALL<-NULL
+for(i in 1:length(filez)){
+  currentWeatherdata<-nc_open(filez[i])
+  
+  londata<-ncvar_get(currentWeatherdata,varid = "lon_bounds")
+  latdata<-ncvar_get(currentWeatherdata,varid = "lat_bounds")
+  timedata<-ncvar_get(currentWeatherdata,varid = "time_bounds")
+  lwe<-ncvar_get(currentWeatherdata,varid = "lwe_thickness")
+  uncertainty<-ncvar_get(currentWeatherdata,varid = "uncertainty")
+  nc_close(currentWeatherdata)
+  
+  numdays<-timedata[2]-timedata[1]
+  cur_seq<-seq(from=as.Date("2002-01-01"),by="day",length.out=mean(timedata))
+  monthvec<-month(cur_seq[length(cur_seq)])
+  yearvec<-year(cur_seq[length(cur_seq)])
+  
+  londata[londata>180]<- londata[londata>180] - 360
+  
+  
+  whichlon<-which((londata[2,]>= -130 & londata[2,]<= -105) | (londata[1,]>= -130 & londata[1,]<= -105))
+  whichlat<-which((latdata[2,]>= 25 & latdata[2,]<= 50) | (latdata[1,]>= 25 & latdata[1,]<= 50))
+  lonvec<-londata[1,whichlon]
+  latvec<-latdata[1,whichlat]
+  posinfo<-expand.grid(latvec,lonvec)
+  
+  colnames(posinfo)<-c("min_lat","min_lon")
+  posinfo$max_lat<-posinfo$min_lat+1
+  posinfo$max_lon<-posinfo$min_lon+1
+  posinfo$lwe<-NA
+  posinfo$uncertainty<-NA
+  posinfo$month<-NA
+  posinfo$year<-NA
+  posinfo$numdays<-NA
+  
+  for(j in 1:nrow(posinfo)){
+    curlonpos<-which(londata[1,]==posinfo$min_lon[j])
+    curlatpos<-which(latdata[1,]==posinfo$min_lat[j])
+    
+    posinfo$lwe[j]<-lwe[curlonpos,curlatpos]
+    posinfo$uncertainty[j]<-uncertainty[curlonpos,curlatpos]
+    posinfo$month[j]<-monthvec
+    posinfo$year[j]<-yearvec
+    posinfo$numdays[j]<-numdays
+  }
+  
+  
+  GRACE_ALL<-rbind(GRACE_ALL,posinfo)
+  write.csv(posinfo,paste0("C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSCALE/DATA/GFZ_MONTHLY/GRACE_GFZ_month_",i,".csv"),row.names = F)
+  print(i)
+  
+}
+
+write.csv(GRACE_ALL,paste0("C:/Users/joeja/Desktop/research_postdoc/GRACE_DOWNSCALE/DATA/GRACE_GFZ_all.csv"),row.names = F)
+
+
+
+
+
+
+
